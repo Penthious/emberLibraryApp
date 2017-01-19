65174cc00cedacfc9b32b48dc74d3613e597861a
Penthious <tleffew1994@gmail.com>
2017-01-19 11:37:10 -0700

    added contact display for admin

--- app/router.js
+++ app/router.js
@@ -2,23 +2,23 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+    location: config.locationType,
+    rootURL: config.rootURL
 });
 
-Router.map(function() {
-  this.route('about');
-  this.route('contact');
+Router.map(function () {
+    this.route('about');
+    this.route('contact');
 
-  this.route('admin', function() {
-    this.route('invitations');
-      this.route('contactslg');
-  });
+    this.route('admin', function () {
+        this.route('invitations');
+        this.route('contacts');
+    });
 
-  this.route('libraries', function() {
-    this.route('new');
-    this.route('edit', {path: '/:library_id/edit'});
-  });
+    this.route('libraries', function () {
+        this.route('new');
+        this.route('edit', { path: '/:library_id/edit' });
+    });
 });
 
 export default Router;
