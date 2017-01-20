88277fa0f4e9a9244a6c86534266c25dedd62f60
Penthious <tleffew1994@gmail.com>
2017-01-19 17:39:17 -0700

    author/books/libraries edit

--- app/models/author.js
+++ app/models/author.js
@@ -3,9 +3,10 @@ import Faker from 'faker';
 
 export default DS.Model.extend({
     name: DS.attr('string'),
-    books: DS.hasMany('book'),
+    books: DS.hasMany('book', {inverse: 'author'}),
 
     isNotValid: Ember.computed.empty('name'),
+
 
     randomize(){
         this.set('name', Faker.name.findName());
---
+++ app/routes/authors.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model(){
+        return this.store.findAll('author');
+    },
+
+    actions: {
+        editAuthor(author){
+            author.set('isEditing', true);
+        },
+
+        cancelAuthorEdit(author){
+            author.set('isEditing', false);
+            author.rollbackAttributes();
+        },
+
+        saveAuthor(author) {
+            if(!author.get('isNotValid')){
+                author.set('isEditing', false);
+                author.save();
+            }
+        }
+    }
+});
